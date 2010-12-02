665fd074e1e8e143e97a56f657fc63e65a172391
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-12-02 02:17:54 +0000

    Default to not building docs when installing gems.

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,2 +1,3 @@
 default.rubygems[:sources] = []
 default.rubygems[:version] = '1.3.6'
+default.rubygems[:default_options] = ""
--- recipes/client.rb
+++ recipes/client.rb
@@ -1,11 +1,9 @@
-if !node[:rubygems][:sources].empty?
-  template "/etc/gemrc" do
-    source "gemrc.erb"
-    mode 0644
-  end
+template "/etc/gemrc" do
+  source "gemrc.erb"
+  mode 0644
 end
 
 execute "update rubygems" do
-  command "gem install rubygems-update -v #{node[:rubygems][:version]} --no-rdoc --no-ri && update_rubygems"
+  command "gem install rubygems-update -v #{node[:rubygems][:version]} && update_rubygems"
   not_if { `gem -v`.chomp == node[:rubygems][:version] }
 end
